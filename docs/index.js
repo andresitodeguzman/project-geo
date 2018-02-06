2ec8de7e152fb7d0a4a6924513db6174a445574b
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-02-06 09:43:11 +0800

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,15 @@
 if("geolocation" in navigator){
   navigator.geolocation.getCurrentPosition(postion=>{
-    $("#position").html(`Position ${position}`);
+    var coords = position.coords;
+    $("#position").html(`
+      Position<br><br>
+      Latitude: ${coords.latitude}<br>
+      Longitude: ${coords.longitude}<br>
+      Accuracy: ${coords.accuracy}<br><br>
+      <a href="http://www.openstreetmap.org/?lat=${coords.latitude}&lon=${coords.longitude}&zoom=17&layers=M">Open Map</a>
+     `);
+  }, err=>{
+    alert(err);
   });
 } else {
   console.error("Geolocation is not available on this device.");
